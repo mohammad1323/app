8ae3cea4da2335b676661016ab58ad14e25950e2
mohammad1323 <mohammadjavadarab1133@gmail.com>
2025-12-29 17:34:10 +0100

    titel

--- app-game/app/page.tsx
+++ app-game/app/page.tsx
@@ -1,21 +1,15 @@
+
 export default function GamePage() {
   return (
     <div className="pt-8 flex flex-col items-center">
-      <h1 className="text-4xl font-extrabold tracking-wide">
-        🎮 App Game
-      </h1>
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold tracking-widest text-green-400">
+          APP GAME
+        </h1>
+        <p className="text-zinc-400 mt-2">
+          Tippe schnell. Gewinne Punkte.
+        </p>
+      </div>
     </div>
   );
 }
-<h1 className="text-4xl font-extrabold tracking-widest text-green-400 drop-shadow-lg">
-  APP GAME
-</h1>
-<div className="text-center">
-  <h1 className="text-4xl font-extrabold tracking-widest text-green-400">
-    APP GAME
-  </h1>
-  <p className="text-zinc-400 mt-2">
-    Tippe schnell. Gewinne Punkte.
-  </p>
-</div>
-
